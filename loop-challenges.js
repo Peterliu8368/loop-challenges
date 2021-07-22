79905aa8842d53b1a71c45488a44587581750015
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-07-22 10:12:41 -0700

    adds the rest of the challenges

--- loop-challenges.js
+++ loop-challenges.js
@@ -1,6 +1,38 @@
-// print odds 1-20
-for (let i = 1; i <= 20; i++) {
-    if(i % 2 != 0) {
-        console.log(i);
-    }
+// 1. print odds 1-20
+// for (let i = 1; i <= 20; i++) {
+//     if(i % 2 != 0) {
+//         console.log(i);
+//     }
+// }
+
+// 2. decreasing multiples of 3
+// for (let i = 100; i >= 0; i--) 
+// {
+//     if (i % 3 == 0)
+//     {
+//         console.log(i);
+//     }
+// }
+
+// 3. print the sequence
+// const arr = [4, 2.5, 1, -0.5, -2, -3.5];
+// for (let i = 0; i < arr.length; i++)
+// {
+//     console.log(arr[i]);
+// }
+
+// 4. Sigma
+// let sum = 0;
+// for (let i = 1; i <= 100; i++)
+// {
+//     sum += i;
+// }
+// console.log(sum);
+
+// 5. Factorial
+let facSum = 1;
+for (let i = 2; i <= 12; i++)
+{
+    facSum *= i;
 }
+console.log(facSum);
